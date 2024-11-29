69c9dd4919f7da904cb147e60891f2bb599e588a
Jatin solanki <jatinsolanki1202@gmail.com>
2024-11-29 14:54:02 +0530

    Update vercel.json

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const app = express()
 connectToDb();
 
 app.set("view engine", "ejs")
+app.set('views', './views');
+app.use(express.static('public'));
 
 app.use(cookieParser())
 app.use(express.json())
